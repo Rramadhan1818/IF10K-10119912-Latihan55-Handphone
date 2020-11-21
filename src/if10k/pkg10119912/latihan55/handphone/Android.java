aa1b45d8ee2c9377fc8576b4ad54e2a8eacb015d
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-21 18:59:56 +0700

    first add

---
+++ src/if10k/pkg10119912/latihan55/handphone/Android.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan55.handphone;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class Android extends Handphone{
+    private String keyStore;
+
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+}
---
+++ src/if10k/pkg10119912/latihan55/handphone/BlackBerry.java
@@ -0,0 +1,29 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan55.handphone;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class BlackBerry extends Handphone{
+    private String pinBB;
+    
+    public BlackBerry(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getPinBB() {
+        return pinBB;
+    }
+
+    public void setPinBB(String pinBB) {
+        this.pinBB = pinBB;
+    }
+}
